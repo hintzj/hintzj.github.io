8e681ab1012b623133a24a3e27a82e5b69e0da2a
Jonathan Hintz <hintz.jonathan@gmail.com>
2023-03-15 23:09:57 +0000

    php Test

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php
+    echo "<div class="endOfPage">\n
+        <adress>\n
+        <p>\n
+        Wassersportverein Lampertheim 1929 e.V. / Albrecht-D&uuml;rer-Stra&szlig;e / 68623 Lampertheim / Hessen, Deutschland\n
+        </p>\n
+        </adress>\n
+        <footer>Copyright 2023</footer>\n
+        </div>";
+?>
