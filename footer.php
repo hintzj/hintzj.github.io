28a2b03b360952066aa4087a7acfb3c592703cb2
hintzj <hintz.jonathan@gmail.com>
2023-03-16 00:33:50 +0100

    added seperate php files for header and footer redirected all pages to use the new header and footer redirected all pages to their new php files

--- footer.php
+++ footer.php
@@ -1,10 +1,14 @@
+<!--This file is included in all pages of the website. It contains the footer of the website.-->
+<!--The footer is the same on all pages, so it is easier to maintain it in one file.-->
 <?php
-    echo "<div class="endOfPage">\n
+
+    echo "<div class='endOfPage'>\n
         <adress>\n
         <p>\n
         Wassersportverein Lampertheim 1929 e.V. / Albrecht-D&uuml;rer-Stra&szlig;e / 68623 Lampertheim / Hessen, Deutschland\n
         </p>\n
         </adress>\n
-        <footer>Copyright 2023</footer>\n
+        <footer>Copyright 1929-" . date("Y") . "</footer>\n
         </div>";
+
 ?>
---
+++ header.php
@@ -0,0 +1,51 @@
+<!-- This is the header of the website. It is included in all pages of the website. -->
+<!-- The header is the same on all pages, so it is easier to maintain it in one file. -->
+
+<?php
+    echo "<header>
+        <div class='header'>
+            <img src='pics/logo1.png'> 
+            <h1>Wassersportverein Lampertheim 1929 e.V.</h1>
+            <nav>
+            <div class='navbar'>                   
+                    <a href='index.php'>Startseite</a>
+    <!--dropdown für die Abteilungen-->
+                        <div class='dropdown-abt'>
+                                <button class='dropbtn-abt'>Abteilungen
+                                    <i class='fa fa-caret-down'></i>
+                                </button>
+                                <div class='dropdown-content-abt'>
+                                    <a href='kanurennsport.php'>Wettkampfsport</a>
+                                    <a href='Fitnesssport.php'>Fitnesssport</a>
+                                    <a href='Kinderturnen.php'>Kinderturnen</a>
+                                    <a href='Kindeswohl.php'>Kindeswohl</a>
+                                </div>
+                        </div>
+    <!-- dropdown für Unser verein-->
+                        <div class='dropdown-uVn'>
+                                <button class='dropbtn-uVn'>Unser Verein
+                                    <i class='fa fa-caret-down'></i>
+                                </button>
+                                <div class='dropdown-content-uVn'>
+                                    <a href='archiv.php'>Archiv</a>
+                                    <a href='termine.php'>Termine</a>
+                                    <a href='vorstand.php'>Vorstand</a>
+                                    <a href='sponosrs.php'>Sponsoren und Förderung</a>
+                                    <a href='links.php'>Nützliche Links</a>
+                                </div>
+                        </div>
+    <!-- dropdown für Vereinsjugend-->
+                        <div class='dropdown-jgn'>
+                                <button class='dropbtn-jgn'>Vereinsjugend
+                                    <i class='fa fa-caret-down'></i>
+                                </button>
+                                <div class='dropdown-content-jgn'>
+                                    <a href='jugendvorstand.php'>Jugendvorstand</a>
+                                    <a href='jugendnews.php'>Jugendnews</a>
+                                </div>
+                        </div>
+                    <a href='https://www.instagram.com/wsv_lampertheim_1929/'><div class='logo-image'><img src='pics/insta_icon_btn.png'></div></a>
+            </div>
+            </nav>
+        </div>
+    </header>";
