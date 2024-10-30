ead7b297d4c8806d6374bdb63f326057a24b2785
Lord_Redstone01 <125308615+LordRedstone01@users.noreply.github.com>
2024-10-31 00:01:18 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,151 +1,173 @@
-<?php
+?php
     include 'functions.php';
     header("SameSite=None; Secure");
 ?>
-<!DOCTYPE HTML>
-<html>
-
-<head>
-    <?php include 'defaultHead.php'; ?>
-    <title>WSV-Lampertheim</title>
-</head>
-
-<body>
-    <?php include 'header.php'; ?>
-    <div class="content">
-        <main>
-            <?php 
-                $filename = getcwd() . $_SERVER['PHP_SELF'];
-                $filename = basename($filename, ".php");
-                $imageFilename = "documents/pics/introImage/" . $filename . ".png";
-                //echo $filename;
-            ?>
-            <div class="greeting" style="background-image: url(<?php echo $imageFilename ?>);" ;>
-                <div class="greeting" style="background-color: rgba(255, 255, 255, 0.75); height: 100%;">
-                    <div>
-                        <h2>Willkommen auf der Website des WSV-Lampertheim</h2>
-                        <p>
-                            Gelegen am schönen Althrein betreiben wir hier am WSV erfolgreichen Kanurennsport und
-                            Kanupolo.
-                            <br>Doch auch ein schönes Vereinsleben und eine gute Jugendarbeit ist uns wichtig.
-                            <br>Die Kanuakademie ist teil unserer Philosophie der Vereinbarung von Sport und Schule
-                            <br>und wirkt als Teilzeitinternat im Bereich der Kindernachmittagsbetreuung.
-                            <br>
-                            <br>
-                            <img src="documents/pics/logo1.png" alt="Logo des Wassersportvereins">
-                        </p>
-
-                    </div>
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="​Willkommen auf der Website des WSV-Lampertheim, Iterative Methods">
+    <meta name="description" content="">
+    <title>Idex</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="index.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 6.16.5, nicepage.com">
+    
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/logo1.png"
+}</script>
+    <meta name="theme-color" content="#5e8ae1">
+    <meta property="og:title" content="Index">
+    <meta property="og:type" content="website">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body data-home-page="Index.html" data-home-page-title="Index" data-path-to-root="./" data-include-products="false" class="u-body u-xl-mode" data-lang="en"><header class="u-align-center-sm u-align-center-xs u-clearfix u-custom-color-2 u-header u-header" id="sec-18a2"><div class="u-clearfix u-sheet u-valign-top-xs u-sheet-1">
+        <div class="u-container-style u-expanded-width u-group u-palette-1-base u-radius u-shape-round u-group-1">
+          <div class="u-container-layout u-valign-middle-md u-valign-middle-sm u-container-layout-1">
+            <h3 class="u-align-center u-custom-font u-font-arial u-headline u-hidden-lg u-hidden-md u-hidden-xl u-text u-text-1">
+              <a href="/" style="font-weight: 700; font-size: 2.25rem;">WSVL<br>
+              </a>
+            </h3>
+            <h3 class="u-headline u-hidden-md u-hidden-sm u-hidden-xs u-text u-text-default u-text-2">
+              <a href="/">Wassersportverein Lampertheim 1929 e.V.</a>
+            </h3>
+            <a href="#" class="u-enable-responsive u-image u-image-round u-logo u-radius u-image-1" data-image-width="285" data-image-height="177">
+              <img src="images/logo1.png" class="u-logo-image u-logo-image-1">
+            </a>
+            <nav class="u-menu u-menu-hamburger u-offcanvas u-menu-1" data-responsive-from="XL">
+              <div class="menu-collapse">
+                <a class="u-button-style u-nav-link" href="#" style="padding: 4px 6px; font-size: calc(1em + 8px);">
+                  <svg class="u-svg-link" viewBox="0 0 24 24"><use xlink:href="#svg-29e8"></use></svg>
+                  <svg class="u-svg-content" version="1.1" id="svg-29e8" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect></g>
+                  </svg>
+                </a>
+              </div>
+              <div class="u-nav-container"></div>
+              <div class="u-nav-container-collapse">
+                <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+                  <div class="u-inner-container-layout u-sidenav-overflow">
+                    <div class="u-menu-close"></div>
+                    <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Home.html">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="About.html">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="./">Idex</a>
+</li></ul>
+                  </div>
                 </div>
-            </div>
-            <div class="text-field1">
-                <h4>Unsere Abteilungen</h4>
-                <p>
-                    <?php
-                        $conn = connect();
-                        if ($conn == false) {
-                            throw new Exception("DB Connection failed");
-                        }
-                        $sql = "SELECT * FROM abteilungen";
-                        $result = $conn->query($sql);
-                        $tableScript = "<table style='width: 100%'>";
-                        $counter = 0;
-                        while($row = $result->fetch_assoc()) {
-                            if ($counter == 0) {
-                                $tableScript .= "<tr>";
-                            }
-                            $tableScript .= "<td>";
-                            $tableScript .= "<a href='" . strtolower($row['abteilungName']) . ".php'>";
-                            $tableScript .= "<img src='documents/pics/introImage/" . strtolower($row['abteilungName']) . ".png' alt='" . $row['abteilungName'] . " style='width='200em'; height='auto'>";
-                            $tableScript .= "</a>";
-                            $tableScript .= "<br>";
-                            $tableScript .= "<a href='" . strtolower($row['abteilungName']) . ".php'>";
-                            $tableScript .= $row['abteilungName'];
-                            $tableScript .= "</a>";
-                            $tableScript .= "</td>";
-                            $counter++;
-                            if ($counter == 3) {
-                                $tableScript .= "</tr>";
-                                $counter = 0;
-                            }
-                        }
-                        $tableScript .= "</table>";
-                        echo $tableScript;
-                    ?>
+                <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+              </div>
+            </nav>
+          </div>
+        </div>
+        <h3 class="u-headline u-hidden-lg u-hidden-sm u-hidden-xl u-hidden-xs u-text u-text-default-md u-text-default-sm u-text-default-xs u-text-3">
+          <a href="/">Wassersportverein Lampertheim</a>
+        </h3>
+      </div></header>
+    <section class="u-align-center u-clearfix u-section-1" id="sec-02db">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-align-center u-container-style u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-group u-image u-image-round u-radius u-shape-round u-image-1" data-image-width="1024" data-image-height="768">
+          <div class="u-container-layout u-valign-middle-md u-valign-middle-sm u-container-layout-1">
+            <div class="u-align-center u-container-style u-expanded-width u-group u-opacity u-opacity-70 u-palette-1-light-2 u-radius u-shape-round u-group-2">
+              <div class="u-container-layout u-container-layout-2">
+                <h2 class="u-text u-text-default u-text-palette-4-base u-text-1"> Willkommen auf der Website des WSV-Lampertheim</h2>
+                <p class="u-text u-text-2"> Gelegen am schönen Althrein betreiben wir hier am WSV erfolgreichen Kanurennsport und Kanupolo.<br>Doch auch ein schönes Vereinsleben und eine gute Jugendarbeit ist uns wichtig.<br>Die Kanuakademie ist teil unserer Philosophie der Vereinbarung von Sport und Schule<br>und wirkt als Teilzeitinternat im Bereich der Kindernachmittagsbetreuung.
                 </p>
+              </div>
             </div>
-            <div class="text-field2">
-                <h4>Vereinsnews</h4>
-                <ul>
-                    <!--
-                 * FILEPATH: /c:/webserver/htdocs/WSV_Webpage/hintzj.github.io/index.php
-                 * This code connects to the "public" database, selects the latest 3 articles from the "artikel" table, and displays them on the webpage.
-                 * Each article is displayed in a div with its title, summary, and a "Weiterlesen" button that links to the full article.
-                 * If there is an error connecting to the database or executing the query, an error message is displayed.
-                -->
-                    <?php
-                    try {
-                        $conn = connect("public");
-                        if ($conn == false) {
-                            throw new Exception("DB Connection failed");
-                        }
-                        $sql = "SELECT * FROM artikel ORDER BY date DESC LIMIT 3";
-                        $result = mysqli_query($conn, $sql);
-                        $articles = mysqli_fetch_all($result, MYSQLI_ASSOC);
-                        mysqli_free_result($result);
-                        mysqli_close($conn);
-                        //make a div for every article that has a read more button that links to the article
-                        foreach ($articles as $article) {
-                            echo "<div class='article'>";
-                            echo "<h3>" . $article['title'] . "</h3>";
-                            echo "<p>" . $article['summary'] . "</p>";
-                            echo "<input type='button' value='Weiterlesen' onclick='window.location.href=\"artikel.php?id=" . $article['artikelID'] . "\"'>";
-                            echo "</div>";
-                            echo "<br>";
-                        }
-                    } catch (Exception $e) {
-                        echo "Error: " . $e->getMessage();
-                        //error_logfile($error, debug_backtrace()[0]['file'].":".debug_backtrace()[0]['line']);
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-align-center u-clearfix u-grey-10 u-section-2" id="sec-fe11">
+      <div class="u-clearfix u-sheet u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-sheet-1">
+        <div class="u-container-style u-expanded-width u-group u-palette-1-base u-radius u-shape-round u-group-1">
+          <div class="u-container-layout u-container-layout-1">
+            <h2 class="u-align-center u-text u-text-palette-3-base u-text-1">Unsere Abteilungen</h2>
+            <p class="u-align-center u-text u-text-palette-3-base u-text-2"> 
+              <?php
+                $conn = connect();
+                if ($conn == false) {
+                    throw new Exception("DB Connection failed");
+                }
+                $sql = "SELECT * FROM abteilungen";
+                $result = $conn->query($sql);
+                $tableScript = "<table style='width: 100%'>";
+                $counter = 0;
+                while($row = $result->fetch_assoc()) {
+                    if ($counter == 0) {
+                        $tableScript .= "<tr>";
                     }
+                    $tableScript .= "<td>";
+                    $tableScript .= "<a href='" . strtolower($row['abteilungName']) . ".php'>";
+                    $tableScript .= "<img src='documents/pics/introImage/" . strtolower($row['abteilungName']) . ".png' alt='" . $row['abteilungName'] . " style='width='200em'; height='auto'>";
+                    $tableScript .= "</a>";
+                    $tableScript .= "<br>";
+                    $tableScript .= "<a href='" . strtolower($row['abteilungName']) . ".php'>";
+                    $tableScript .= $row['abteilungName'];
+                    $tableScript .= "</a>";
+                    $tableScript .= "</td>";
+                    $counter++;
+                    if ($counter == 3) {
+                        $tableScript .= "</tr>";
+                        $counter = 0;
+                    }
+                }
+                $tableScript .= "</table>";
+                echo $tableScript;
+              ?>
+            </p>
+            <h2 class="u-align-center u-text u-text-palette-3-base u-text-1">Vereinsnews</h2>
+            <p class="u-align-center u-text u-text-palette-3-base u-text-2"> 
+              <!--
+                * FILEPATH: /c:/webserver/htdocs/WSV_Webpage/hintzj.github.io/index.php
+                * This code connects to the "public" database, selects the latest 3 articles from the "artikel" table, and displays them on the webpage.
+                * Each article is displayed in a div with its title, summary, and a "Weiterlesen" button that links to the full article.
+                * If there is an error connecting to the database or executing the query, an error message is displayed.
+              -->
+              <?php
+                  try {
+                      $conn = connect("public");
+                      if ($conn == false) {
+                          throw new Exception("DB Connection failed");
+                      }
+                      $sql = "SELECT * FROM artikel ORDER BY date DESC LIMIT 3";
+                      $result = mysqli_query($conn, $sql);
+                      $articles = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                      mysqli_free_result($result);
+                      mysqli_close($conn);
+                      //make a div for every article that has a read more button that links to the article
+                      foreach ($articles as $article) {
+                          echo "<div class='article'>";
+                          echo "<h3>" . $article['title'] . "</h3>";
+                          echo "<p>" . $article['summary'] . "</p>";
+                          echo "<input type='button' value='Weiterlesen' onclick='window.location.href=\"artikel.php?id=" . $article['artikelID'] . "\"'>";
+                          echo "</div>";
+                          echo "<br>";
+                      }
+                  } catch (Exception $e) {
+                      echo "Error: " . $e->getMessage();
+                      //error_logfile($error, debug_backtrace()[0]['file'].":".debug_backtrace()[0]['line']);
+                  }
                 ?>
-                </ul>
-                <div style="text-align: center;">
-                    Falls du immer auf dem neusten Stand bleiben willst, kannst du dich auch für unseren WhatsApp
-                    verteiler anmelden.<br><br>
-                    <a href="https://chat.whatsapp.com/IeHYUxnAh47HMNSKh4twUG">
-                    <img src="documents/pics/whatsAppInvite.png" alt="QR-Code für den WhatsApp Verteiler"
-                        style="width: 20%; margin: inherit;">
-                    </a>
-                </div>
-            </div>
-            <div class="text-field3">
-                <h4>Schnupperzeiten</h4>
-                <p>
-                <ul>
-                    Bei uns sind neue Gesichter immer Willkommen. Wenn du Interesse hast, kannst du gerne zu unseren
-                    Schnupperzeiten kommen.
-                    <br>
-                    <br>
-                    <b>Kanurennsport:</b> Montags 16.30 - 18.00 Uhr
-                    <br>
-                    <b>Kanupolo:</b> Dienstags 18.00 - 20.00 Uhr
-                    <br>
-                    <b>Wanderpaddeln:</b> Mittwochs 18.00 - 20.00 Uhr
-                    <br>
-                    <b>Rückengymnastik:</b> Mittwochs 09.00 - 10.30 Uhr
-                    <br>
-                    <b>Yoga:</b> Freitags 18.30 - 20.00 Uhr
-                    <br>
-                    <b>Boule:</b> Freitags 15.00 - 17.00 Uhr
-                    <br>
-                    <b>Bodyforming:</b> Freitags 20.00 - 21.00 Uhr
-                </ul>
-                </p>
-            </div>
-        </main>
-        <?php include "footer.php"; ?>
-    </div>
-    <?php include "wavesFooter.php"; ?>
-</body>
-
-</html>
+            </p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-palette-1-base u-footer" id="sec-2334"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-align-center-md u-align-center-sm u-align-center-xs u-small-text u-text u-text-palette-3-base u-text-variant u-text-1"> Wassersportverein Lampertheim 1929 e.V. / Albrecht-Dürer-Straße 46 / 68623 Lampertheim / Hessen, Deutschland</p>
+        <p class="u-small-text u-text u-text-palette-3-base u-text-variant u-text-2">
+          <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-3-base u-btn-1" data-href="#"> Copyright 1929-2024 /&nbsp;Impressum</a>
+        </p>
+      </div></footer>
+</body></html>
